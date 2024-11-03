c3dea4f7a9364280e950705f363899d622eefd1c
oscar garcia <oscargarcia@oscars-MacBook-Pro.local>
2024-11-03 01:09:18 -0700

    Initial commit

---
+++ src/Jokeyjokes.java
@@ -0,0 +1,21 @@
+public class Jokeyjokes {
+    public static void main(String[] args) {
+        System.out.println("Me   : Knock knock!");
+
+        System.out.println("You  : Who's there?");
+
+        System.out.println("Me   : Dorris");
+
+        System.out.println("You  : Dorris who?");
+
+        System.out.println("Me   : Dorris locked, that's why I'm knocking");
+
+        System.out.println("You  : ...");
+
+        System.out.println("Me   : Hah...get it? Cuz I'm knocking!");
+
+        System.out.println("You  : ...");
+
+        System.out.println("You  : This is why I'm your only friend");
+    }
+}
